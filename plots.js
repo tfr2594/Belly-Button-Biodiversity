4eaeb43af8231e7c21f4477108b50e66bfbd54dd
tfr2594 <tfr2594@gmail.com>
2022-01-15 18:13:32 -0500

    working bar graph it works

--- plots.js
+++ plots.js
@@ -2,17 +2,45 @@ console.log("This is plot.js")
 
 function DrawBarchart(sampleId) {
     console.log(`DrawBarchart(${sampleId})`);
+
+    d3.json("samples.json").then(data => {
+
+        let samples = data.samples;
+        let resultArray = samples.filter(s => s.id === sampleId);
+        let result = resultArray[0];
+
+        console.log(result)
+
+        let otu_ids = result.otu_ids;
+        let otu_lables = result.otu_labels;
+        let sample_values = result.sample_values;
+        let yticks = otu_ids.slice(0,10).map(otuId => `OTU${otuId}`).reverse();
+
+        let barData = {
+            x: sample_values.slice(0, 10).reverse(),
+            y: yticks,
+            type: "bar",
+            text: otu_lables.slice(0, 10).reverse(),
+            orientation: "h",
+        };
+        let barArray = [barData];
+
+        let barLayout = {
+            title: "Top 10 Bacteria Cultures Found",
+            margin: {t: 30, l: 150}
+        }
+
+        Plotly.newPlot("bar", barArray, barLayout);
+    });
 }
 
 
 function DrawBubblechart(sampleId) {
     console.log(`DrawBubblechat(${sampleId})`);
-
 }
 
 function ShowMetadata(sampleId) {
     console.log(`ShowMetadata(${sampleId})`);
-
 }
 
 
@@ -34,8 +62,6 @@ function InitDashboard()
 
     d3.json("samples.json").then(data=> {
 
-        console.log(data);
-
         let sampleNames = data.names;
 
         sampleNames.forEach(sampleId => {
@@ -45,6 +71,12 @@ function InitDashboard()
                 .property("value", sampleId);
 
         });
+
+        let sampleId = sampleNames[0];
+
+        DrawBubblechart(sampleId);
+        DrawBubblechart(sampleId);
+        ShowMetadata(sampleId);
     });
 }
 
