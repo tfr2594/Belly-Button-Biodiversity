b5fd262763332625c12fe546b4af58bd98356935
tfr2594 <tfr2594@gmail.com>
2022-01-15 17:36:23 -0500

    added fuction stubs

--- plots.js
+++ plots.js
@@ -1,5 +1,28 @@
 console.log("This is plot.js")
 
+function DrawBarchart(sampleId) {
+    console.log(`DrawBarchart(${sampleId})`);
+}
+
+
+function DrawBubblechart(sampleId) {
+    console.log(`DrawBubblechat(${sampleId})`);
+
+}
+
+function ShowMetadata(sampleId) {
+    console.log(`ShowMetadata(${sampleId})`);
+
+}
+
+
+function optionChanged(id) {
+    console.log(`optionChanged(${id})`);
+
+    DrawBarchart(id);
+    DrawBubblechart(id);
+    ShowMetadata(id);
+}
 
 //Taken from dom's office hour session December 11th
 
@@ -22,9 +45,7 @@ function InitDashboard()
                 .property("value", sampleId);
 
         });
-
     });
-
 }
 
 InitDashboard();
